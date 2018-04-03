625a0e2a6ef4de673a463b947ba1c3865031910d
md678685 <mathew678@hotmail.co.uk>
2018-04-03 20:47:11 +0100

    Add constants class

---
+++ src/main/java/org/moss/discord/Constants.java
@@ -0,0 +1,25 @@
+package org.moss.discord;
+
+/*
+ * Holds IDs for the guild, channels and roles.
+ */
+public class Constants {
+
+    // Guilds
+    public static final String GUILD_MOSS = "390942438061113344";
+
+    // Channels
+
+    public static final String CHANNEL_STARBOARD = ""; // TODO: fill these in once created
+    public static final String CHANNEL_MODLOG = "";
+
+    // Roles
+
+    public static final String ROLE_MODERATOR = "390944427071242240";
+    
+    public static final String ROLE_PURCHASER = "401686955336663041";
+    public static final String ROLE_PURCHASER_PVX = "401687126242230274";
+    public static final String ROLE_PURCHASER_FUUID = "401687069048700928";
+    public static final String ROLE_PURCHASER_OD = "401687160559763456";
+
+}
