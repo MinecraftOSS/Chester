11c2d29f3f486e7620bfaff51333cf7ec87d8504
U-NIFLHEIM\Lax <LaxWasHere@PoweredByHate.net>
2018-04-04 03:22:34 -0700

    Fancier embed

--- src/main/java/org/moss/discord/listeners/ModLogListeners.java
+++ src/main/java/org/moss/discord/listeners/ModLogListeners.java
@@ -2,6 +2,8 @@ package org.moss.discord.listeners;
 
 import org.javacord.api.DiscordApi;
 import org.javacord.api.entity.channel.TextChannel;
+import org.javacord.api.entity.message.Message;
+import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.javacord.api.event.message.MessageDeleteEvent;
 import org.javacord.api.event.message.MessageEditEvent;
 import org.javacord.api.event.server.member.ServerMemberBanEvent;
@@ -14,6 +16,8 @@ import org.javacord.api.listener.server.member.ServerMemberJoinListener;
 import org.javacord.api.listener.server.member.ServerMemberLeaveListener;
 import org.moss.discord.Constants;
 
+import java.awt.*;
+import java.time.Instant;
 import java.util.Optional;
 
 public class ModLogListeners implements MessageEditListener, MessageDeleteListener, ServerMemberBanListener, ServerMemberJoinListener, ServerMemberLeaveListener {
@@ -25,7 +29,6 @@ public class ModLogListeners implements MessageEditListener, MessageDeleteListen
     ++ User Join/Leave
     +- User Ban/Kick
     ++ User Edit/Delete message
-    TODO: Make prettier with emojis?
      */
 
     public ModLogListeners(DiscordApi api) {
@@ -34,30 +37,87 @@ public class ModLogListeners implements MessageEditListener, MessageDeleteListen
     }
 
     @Override
-    public void onMessageDelete(MessageDeleteEvent ev) { //TODO Ignore if the user is not present. = Banned & Fix channels
-        String messageFormat = "**Delete** \n **Chan:** %s \n **Author** %s \n **Message:** %s";
-        modChannel.get().sendMessage(String.format(messageFormat, ev.getMessage().get().getChannel(), ev.getMessage().get().getUserAuthor().get().getMentionTag(), ev.getMessage().get().getContent()));
+    public void onMessageDelete(MessageDeleteEvent ev) {
+        try {
+            Message message = ev.getMessage().orElseGet(null); //TODO
+        } catch (Exception e) {
+            return;
+        }
+
+        EmbedBuilder embed = new EmbedBuilder();
+
+        embed.setAuthor("DELETE");
+        embed.setColor(Color.RED);
+        embed.setThumbnail("https://i.imgur.com/bYGnGCp.png");
+
+        embed.addInlineField("Author", ev.getMessage().get().getAuthor().getDiscriminatedName());
+        embed.addInlineField("Channel", String.format("<#%s>", ev.getChannel().getId()));
+
+        embed.addField("Message", "```"+ev.getMessage().get().getContent()+"```");
+
+        embed.setFooter("Time");
+        embed.setTimestamp(ev.getMessage().get().getCreationTimestamp());
+
+        modChannel.get().sendMessage(embed);
     }
 
     @Override
     public void onMessageEdit(MessageEditEvent ev) {
-        String messageFormat = "**Edit** \n **Chan:** %s \n **Author** %s \n **Was:** %s \n **Now:** %s";
-        modChannel.get().sendMessage(String.format(messageFormat, ev.getChannel(), ev.getMessage().get().getUserAuthor().get().getMentionTag(), ev.getOldContent().get(), ev.getNewContent()));
+        EmbedBuilder embed = new EmbedBuilder();
+
+        embed.setAuthor("EDIT");
+        embed.setColor(Color.YELLOW);
+        embed.setThumbnail("https://i.imgur.com/bYGnGCp.png");
+
+        embed.addInlineField("Author", ev.getMessage().get().getAuthor().getDiscriminatedName());
+        embed.addInlineField("Channel", String.format("<#%s>", ev.getChannel().getId()));
+
+        embed.addField("Was", "```"+ev.getOldContent().get()+"```");
+        embed.addField("Now", "```"+ev.getNewContent()+"```");
+
+        embed.setFooter("Time");
+        embed.setTimestamp(ev.getMessage().get().getCreationTimestamp());
+
+        modChannel.get().sendMessage(embed);
     }
 
     @Override
-    public void onServerMemberBan(ServerMemberBanEvent ev) { //TODO Get by/reason from audit log
-        String banMessage = "**BAN** (%s) \n **User:** %s %s \n **Reason**: %s \n **By:** %s";
-        modChannel.get().sendMessage(String.format(banMessage, ev.getUser().getIdAsString(), ev.getUser().getDiscriminatedName(), ev.getUser().getMentionTag(), "Reason", "Me"));
+    public void onServerMemberBan(ServerMemberBanEvent ev) {
+        EmbedBuilder embed = new EmbedBuilder();
+
+        embed.setAuthor(ev.getUser());
+        embed.setColor(Color.YELLOW);
+        embed.setThumbnail("https://i.imgur.com/8WJqz7B.png");
+
+        embed.addInlineField("Banned By: ", "Chester"); //TODO
+        embed.addInlineField("ID", ev.getUser().getIdAsString());
+        embed.addField("Reason", "Audit Log");
+
+        embed.setFooter("Banned");
+        embed.setTimestamp(Instant.now());
+
+        modChannel.get().sendMessage(embed);
     }
 
     @Override
     public void onServerMemberJoin(ServerMemberJoinEvent ev) {
-        modChannel.get().sendMessage("**Join** " + ev.getUser().getMentionTag());
+        EmbedBuilder embed = new EmbedBuilder();
+
+        embed.setAuthor(ev.getUser());
+        embed.setTitle("Joined the server");
+        embed.setColor(Color.GREEN);
+
+        modChannel.get().sendMessage(embed);
     }
 
     @Override
     public void onServerMemberLeave(ServerMemberLeaveEvent ev) {
-        modChannel.get().sendMessage("**Leave** " + ev.getUser().getMentionTag());
+        EmbedBuilder embed = new EmbedBuilder();
+
+        embed.setAuthor(ev.getUser());
+        embed.setTitle("Left the server");
+        embed.setColor(Color.RED);
+
+        modChannel.get().sendMessage(embed);
     }
 }
