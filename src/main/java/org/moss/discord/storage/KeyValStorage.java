554ff244c704f15ba2c380b66d1d1dd6f07f6d87
md678685 <mathew678@hotmail.co.uk>
2018-04-04 13:31:41 +0100

    Implement simple YAML-backed key-value storage

---
+++ src/main/java/org/moss/discord/storage/KeyValStorage.java
@@ -0,0 +1,71 @@
+package org.moss.discord.storage;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.yaml.snakeyaml.Yaml;
+
+public class KeyValStorage {
+    
+    private Yaml yaml = new Yaml();
+    private Logger logger = LoggerFactory.getLogger(KeyValStorage.class);
+
+    private Map<String, String> kvMap;
+    private File kvFile;
+
+    public KeyValStorage(String path) {
+        kvFile = new File(path);
+        loadYaml();
+    }
+
+    protected synchronized void loadYaml() {
+        try {
+            kvFile.createNewFile();
+            kvMap = (Map<String, String>) yaml.load(new FileReader(kvFile));
+        } catch (IOException e) {
+            logger.error("Could not load key-value file!", e);
+        }
+
+        if (kvMap == null) kvMap = new HashMap<String, String>();
+
+        logger.debug("Loaded from {}: {}", kvFile.getPath(), kvMap);
+    }
+
+    protected synchronized void saveYaml() {
+        try {
+            yaml.dump(kvMap, new FileWriter(kvFile));
+        } catch (IOException e) {
+            logger.error("Could not save key-value file!", e);
+        }
+    }
+
+    public boolean exists(String key) {
+        return kvMap.containsKey(key);
+    }
+
+    public boolean existsValue(String value) {
+        return kvMap.containsValue(value);
+    }
+
+    public boolean set(String key, String value) {
+        boolean hasKey = exists(key);
+        kvMap.put(key, value);
+        return hasKey;
+    }
+
+    public String get(String key) {
+        return kvMap.get(key);
+    }
+
+    public Map<String, String> getMap() {
+        return Collections.unmodifiableMap(kvMap);
+    }
+
+}
