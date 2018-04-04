a6bdb53430b92f96e09ab5e7fba542588d7a6acf
md678685 <mathew678@hotmail.co.uk>
2018-04-04 16:14:02 +0100

    Tidy up imports

--- src/main/java/org/moss/discord/storage/StarboardStorage.java
+++ src/main/java/org/moss/discord/storage/StarboardStorage.java
@@ -1,14 +1,5 @@
 package org.moss.discord.storage;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Map;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.yaml.snakeyaml.Yaml;
-
 public class StarboardStorage extends KeyValStorage {
 
     public StarboardStorage() {
