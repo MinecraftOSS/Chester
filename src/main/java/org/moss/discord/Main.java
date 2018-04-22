bf0e5ce0363807cbba4190357ab121df2908c891
U-NIFLHEIM\Lax <LaxWasHere@PoweredByHate.net>
2018-04-22 06:24:38 -0700

    Register listener

--- src/main/java/org/moss/discord/Main.java
+++ src/main/java/org/moss/discord/Main.java
@@ -9,6 +9,7 @@ import org.moss.discord.commands.MojangCommand;
 import org.moss.discord.commands.moderation.BanCommand;
 import org.moss.discord.commands.moderation.KickCommand;
 import org.moss.discord.commands.moderation.PruneCommand;
+import org.moss.discord.listeners.AutoModListeners;
 import org.moss.discord.listeners.ModLogListeners;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -44,6 +45,7 @@ public class Main {
 
         // Register listeners
         api.addListener(new ModLogListeners(api));
+        api.addListener(new AutoModListeners(api));
     }
 
 }
