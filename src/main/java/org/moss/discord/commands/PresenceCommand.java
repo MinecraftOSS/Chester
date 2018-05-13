bbf359c0a8d66eecaae2533d5e2d85310515897b
U-NIFLHEIM\Lax <LaxWasHere@PoweredByHate.net>
2018-05-13 06:50:54 -0700

    git amend no work

--- src/main/java/org/moss/discord/commands/PresenceCommand.java
+++ src/main/java/org/moss/discord/commands/PresenceCommand.java
@@ -6,31 +6,26 @@ import de.btobastian.sdcf4j.CommandExecutor;
 import org.apache.commons.lang.ArrayUtils;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.entity.activity.ActivityType;
+import org.javacord.api.entity.channel.TextChannel;
 import org.javacord.api.entity.permission.Role;
 import org.javacord.api.entity.server.Server;
 import org.javacord.api.entity.user.User;
 import org.moss.discord.Constants;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class PresenceCommand implements CommandExecutor {
 
     @Command(aliases = {"!presence", ".presence"}, usage = "!presence <Sstatus>", description = "Sets the status presence of the bot")
-    public void onCommand(DiscordApi api, String[] args, User user, Server server) {
+    public void onCommand(DiscordApi api, String[] args, TextChannel channel, User user, Server server) {
         if (hasPermission(user.getRoles(server)) && args.length >= 2) {
-            String type = args[0].toLowerCase();
+            String type = args[0].toUpperCase();
             String status = String.join(" ", (String[]) ArrayUtils.remove(args, 0));
-            if (type.equals("listening")) {
-                api.updateActivity(String.join(" ", status), ActivityType.LISTENING);
-            }
-            if (type.equals("playing")) {
-                api.updateActivity(String.join(" ", status), ActivityType.PLAYING);
-            }
-            if (type.equals("watching")) {
-                api.updateActivity(String.join(" ", status), ActivityType.WATCHING);
-            }
-            if (type.equals("streaming")) {
-                api.updateActivity(String.join(" ", status), ActivityType.STREAMING);
+            try {
+                api.updateActivity(String.join(" ", status), ActivityType.valueOf(type));
+            } catch (Exception e) {
+                channel.sendMessage("Invalid activity use: " + Arrays.toString(ActivityType.values()));
             }
         }
     }
