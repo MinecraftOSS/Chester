ba18a6cac04b2d07c942858988950d062eb14c47
U-NIFLHEIM\Lax <LaxWasHere@PoweredByHate.net>
2019-02-03 07:41:12 -0800

    advanced embed

--- src/main/java/org/moss/discord/commands/TagCommand.java
+++ src/main/java/org/moss/discord/commands/TagCommand.java
@@ -2,6 +2,7 @@ package org.moss.discord.commands;
 
 import de.btobastian.sdcf4j.Command;
 import de.btobastian.sdcf4j.CommandExecutor;
+import org.apache.commons.lang.ArrayUtils;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.entity.channel.TextChannel;
 import org.javacord.api.entity.message.embed.Embed;
@@ -14,6 +15,7 @@ import org.javacord.api.listener.message.MessageCreateListener;
 import org.moss.discord.Constants;
 import org.moss.discord.storage.FactoidStorage;
 import org.moss.discord.util.EmbedUtil;
+import org.moss.discord.util.KeywordsUtil;
 
 import java.awt.*;
 import java.util.Arrays;
@@ -24,37 +26,39 @@ import java.util.StringJoiner;
 public class TagCommand implements CommandExecutor, MessageCreateListener {
 
     private FactoidStorage storage = new FactoidStorage();
+    EmbedUtil util = new EmbedUtil();
 
     public TagCommand(DiscordApi api) {
         api.addListener(this);
     }
 
-    @Command(aliases = {"!tag", "?tag", "?"}, usage = "!tag <name>", description = "Send the tag message to the channel.")
-    public void onTag(DiscordApi api, TextChannel channel, String[] args) {
-        if (args.length >= 1  && storage.isFactoid(args[0].toLowerCase())) {
-            if (args.length > 1) {
-                String[] tagArgs = Arrays.copyOfRange(args, 1, args.length);
-                try {
-                    channel.sendMessage(String.format(getFactoid(args[0]), (Object[]) tagArgs));
-                } catch (IllegalFormatException e) {
-                    // Some Joker -_- passed in some weird arguments
-                    return;
-                }
-                return;
-            }
-            channel.sendMessage(getFactoid(args[0]));
-        }
-    }
+//    @Command(aliases = {"!tag", "?tag", "?"}, usage = "!tag <name>", description = "Send the tag message to the channel.")
+//    public void onTag(DiscordApi api, TextChannel channel, String[] args) {
+//        if (args.length >= 1  && storage.isFactoid(args[0].toLowerCase())) {
+//            if (args.length > 1) {
+//                String[] tagArgs = Arrays.copyOfRange(args, 1, args.length);
+//                try {
+//                    channel.sendMessage(String.format(getFactoid(args[0]), (Object[]) tagArgs));
+//                } catch (IllegalFormatException e) {
+//                    // Some Joker -_- passed in some weird arguments
+//                    return;
+//                }
+//                return;
+//            }
+//            channel.sendMessage(getFactoid(args[0]));
+//        }
+//    }
 
     @Command(aliases = {"!tagraw", "?tagraw"}, usage = "!tagraw <name>", description = "Send the raw tag message to the channel.")
     public void onTagRaw(DiscordApi api, TextChannel channel, String[] args, User user, Server server) {
-        if (args.length >= 1  && storage.isFactoid(args[0].toLowerCase()) && hasPermission(user.getRoles(server))) {
+        if (args.length >= 1  && storage.isFactoid(args[0].toLowerCase()) && server.canKickUsers(user)) {
             channel.sendMessage("```"+getFactoid(args[0])+"```");
         }
     }
 
     @Command(aliases = {"!tags", "?tags"}, usage = "!tags [filter]", description = "List all currently enabled tags.")
-    public void onList(DiscordApi api, TextChannel channel, String[] args) {
+    public void onList(DiscordApi api, TextChannel channel, Server server, User user) {
+        if (!server.canKickUsers(user)) return;
         String s = "";
         for (String key : storage.getMap().keySet()) {
             s += String.format("`%s` ", key);
@@ -64,7 +68,7 @@ public class TagCommand implements CommandExecutor, MessageCreateListener {
 
     @Command(aliases = {"!tagset", "?tagset"}, usage = "!tagset <name> [message]", description = "Set a new tag")
     public void onSet(DiscordApi api, TextChannel channel, String[] args, User user, Server server) {
-        if (args.length >= 2 && hasPermission(user.getRoles(server))) {
+        if (args.length >= 2 && server.canKickUsers(user)) {
             StringJoiner sb = new StringJoiner(" ");
             for(int i = 1; i < args.length; i++) {
                 sb.add(args[i]);
@@ -76,34 +80,33 @@ public class TagCommand implements CommandExecutor, MessageCreateListener {
 
     @Command(aliases = {"!tagunset", "?tagunset"}, usage = "!tagunset <name> [message]", description = "Unset a tag")
     public void onUnset(DiscordApi api, TextChannel channel, String[] args, User user, Server server) {
-        if (args.length >= 1 && hasPermission(user.getRoles(server))) {
+        if (args.length >= 1 && server.canKickUsers(user)) {
             storage.unset(args[0].toLowerCase());
             channel.sendMessage(new EmbedBuilder().setTitle("Tag removed!").setColor(Color.GREEN));
         }
-    }
-
-    public Boolean hasPermission(List<Role> roles) {
-        for (Role role : roles) {
-            String roleId = role.getIdAsString();
-            if ((roleId.equals(Constants.ROLE_MODERATOR) || roleId.equals(Constants.ROLE_ADMIN) || roleId.equals(Constants.ROLE_PROJECT_LEAD))) {
-                return true;
-            }
-        }
-        return false;
     }
 
     @Override
     public void onMessageCreate(MessageCreateEvent ev) {
         String message = ev.getMessage().getContent();
         if (message.startsWith("?") && message.length() >= 2) {
-            String tag = message.split(" ")[0].substring(1).toLowerCase();
+            String[] args = message.split(" ");
+            String tag = args[0].substring(1).toLowerCase();
+            args = (String[]) ArrayUtils.remove(args, 0);
             if (storage.isFactoid(tag)) {
                 String factoid = getFactoid(tag);
-                if (getFactoid(tag).startsWith("<embed>")) {
-                    EmbedUtil util = new EmbedUtil();
-                    ev.getChannel().sendMessage(util.fromJson(factoid.substring(7)));
+                if (factoid.startsWith("<staff>")) {
+                    factoid = factoid.substring(7);
+                    if (!ev.getMessageAuthor().canKickUsersFromServer()) return;
+                }
+                if (factoid.startsWith("<del>")) {
+                    ev.getMessage().delete();
+                    factoid = factoid.substring(5);
+                }
+                if (factoid.startsWith("<embed>") || factoid.startsWith("<json>")) {
+                    ev.getChannel().sendMessage(util.parseString(factoid, ev.getMessage().getUserAuthor().get(), ev.getServer().get(), args));
                 } else {
-                    ev.getChannel().sendMessage(factoid);
+                    ev.getChannel().sendMessage(new KeywordsUtil(factoid, ev.getMessage().getUserAuthor().get(), ev.getServer().get(), args).replace());
                 }
             }
         }
--- src/main/java/org/moss/discord/util/EmbedUtil.java
+++ src/main/java/org/moss/discord/util/EmbedUtil.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
+import org.javacord.api.entity.server.Server;
+import org.javacord.api.entity.user.User;
 import org.javacord.core.entity.message.embed.EmbedImpl;
 
 import java.awt.*;
@@ -23,7 +25,6 @@ public class EmbedUtil {
      */
     private static final OkHttpClient client = new OkHttpClient.Builder().build();
 
-
     public EmbedBuilder fromJson(String url) {
         try {
             JsonNode jsonEmbed = makeRequest(url);
@@ -32,7 +33,38 @@ public class EmbedUtil {
             e.printStackTrace();
         }
         return new EmbedBuilder().setColor(Color.RED).setTitle("Invalid embed");
+    }
 
+    public EmbedBuilder fromJson(String url, User user, Server server, String... args) {
+        try {
+            JsonNode jsonEmbed = makeRequest(url);
+            return new EmbedImpl(jsonAsString(new KeywordsUtil(jsonEmbed.get("embed").toString(), user, server, args).replace())).toBuilder();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new EmbedBuilder().setColor(Color.RED).setTitle("Invalid embed");
+    }
+
+    public EmbedBuilder fromString(String json, User user, Server server, String... args) {
+        try {
+            return new EmbedImpl(jsonAsString(new KeywordsUtil(json, user, server, args).replace())).toBuilder();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new EmbedBuilder().setColor(Color.RED).setTitle("Invalid embed");
+    }
+
+    public EmbedBuilder parseString(String message, User user, Server server, String... args) {
+        if (message.startsWith("<embed>")) {
+            return fromJson(message.substring(7), user, server, args);
+        }
+        if (message.startsWith("<json>")) {
+            return fromString(message.substring(6), user, server, args);
+        }
+        if (message.startsWith("http")) {
+            return fromJson(message, user, server, args);
+        }
+        return new EmbedBuilder().setColor(Color.RED).setTitle("Invalid embed");
     }
 
     /**
@@ -48,5 +80,9 @@ public class EmbedUtil {
                 .url(url)
                 .build();
         return mapper.readTree(Objects.requireNonNull(client.newCall(request).execute().body()).string());
+    }
+
+    private JsonNode jsonAsString(String jsonString) throws IOException {
+        return mapper.readTree(Objects.requireNonNull(jsonString));
     }
 }
