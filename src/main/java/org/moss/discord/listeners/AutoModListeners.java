51b5293a8a9d714426052dfa1b767934dc31d7a5
Lax <LaxWasHere@PoweredByHate.net>
2019-07-27 21:45:32 -0700

    No ping pls (#35)
    * no ping pls
    
    * exluded channels
    
    * Add #ezprotector to the exclusion list
    
    * exclude lwc (#28)
    
    * crowdin msgs
    
    * add chester's friends

--- src/main/java/org/moss/discord/listeners/AutoModListeners.java
+++ src/main/java/org/moss/discord/listeners/AutoModListeners.java
@@ -2,7 +2,9 @@ package org.moss.discord.listeners;
 
 import org.javacord.api.DiscordApi;
 import org.javacord.api.entity.channel.TextChannel;
+import org.javacord.api.entity.message.Message;
 import org.javacord.api.entity.message.MessageAttachment;
+import org.javacord.api.entity.message.embed.Embed;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.javacord.api.entity.user.User;
 import org.javacord.api.event.message.MessageCreateEvent;
@@ -10,11 +12,18 @@ import org.javacord.api.listener.message.MessageCreateListener;
 import org.moss.discord.Constants;
 
 import java.awt.*;
+import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -24,22 +33,65 @@ public class AutoModListeners implements MessageCreateListener {
     List<String> blacklistedFiles;
     List<String> censoredWords;
     private Optional<TextChannel> modChannel;
+    private Map<Long, Instant> active = new HashMap<>();
+    String[] donts = {
+            "Do not",
+            "Don't",
+            "Stop",
+            "Huge Mistake",
+            "Bad Idea",
+            "No no no",
+            "apologize",
+            "Say sorry",
+            "You will now be terminated",
+            "Prepare for your termination",
+            "We're coming for you",
+            "You've been added to the naughty list",
+            "EXTERMINATE!",
+            "The ban-hammer awaits you"
+    };
 
-    /*
-    TODO: Add role bypass
-     */
+    String[] nopes = {
+            "https://i.imgur.com/z8UBrh5.png",
+            "https://i.imgur.com/J015ZK5.png",
+            "https://i.imgur.com/RJAtyAg.png",
+            "https://i.imgur.com/aPVrnKl.gif",
+            "https://media0.giphy.com/media/rFvtiIevmj0zu/giphy.gif",
+            "https://i.imgur.com/dTMWtkp.png",
+            "https://i.imgur.com/KbsSxlR.gif",
+            "https://i.imgur.com/8r5mriG.jpg"
+    };
+
+    List<String> nodonts = new ArrayList<>(Arrays.asList("430125651559710720", "430125667062120449", "442304736587415574", "479919881702342676", "479919848177139713"));
 
     public AutoModListeners(DiscordApi api) {
         this.api = api;
         modChannel = api.getTextChannelById(Constants.CHANNEL_MODLOG);
         blacklistedFiles = new ArrayList<>(Arrays.asList(".jar", ".exe", ".zip")); //TODO configurable
-        censoredWords = new ArrayList<>(Arrays.asList("discord.gg", "discordapp.com/invite", "blackspigot"));
+        censoredWords = new ArrayList<>(Arrays.asList("discord.gg", "discordapp.com/invite", "blackspigot", "amazingsexdating.com", "whatsappx.com", "bestoffersx.com", "kidsearncash.com", "l10n\\_2\\.x", "38875", "getbukkit"));
     }
 
     @Override
     public void onMessageCreate(MessageCreateEvent ev) {
         if (ev.getMessage().getAuthor().isYourself() || ev.getMessage().getAuthor().canKickUsersFromServer()) {
+            active.put(ev.getMessage().getAuthor().getId(), Instant.now());
             return;
+        }
+
+        parsePings(ev.getMessage());
+
+        if (!ev.getMessage().getEmbeds().isEmpty()) {
+            for (Embed embed : ev.getMessage().getEmbeds()) {
+                String title = embed.getTitle().orElse("");
+                System.out.println(title);
+                for (String pattern : censoredWords) {
+                    if (title.toLowerCase().contains(pattern)) {
+                        ev.getMessage().delete("Pattern trigger: " + pattern);
+                        logCensorMessage(ev.getMessage().getUserAuthor(), pattern, ev.getChannel().getIdAsString());
+                        return;
+                    }
+                }
+            }
         }
 
         for (MessageAttachment messageAttachment : ev.getMessage().getAttachments()) {
@@ -60,6 +112,30 @@ public class AutoModListeners implements MessageCreateListener {
                 return;
             }
         }
+    }
+
+    public void parsePings(Message message) {
+        if (message.getMentionedUsers().size() >= 1 && !nodonts.contains(message.getChannel().getIdAsString())) {
+            for (User user : message.getMentionedUsers()) {
+                if (message.getServer().get().canKickUsers(user) && !userIsActive(user.getId())) {
+                    EmbedBuilder embed = new EmbedBuilder();
+                    embed.setColor(Color.RED);
+                    embed.setImage(nopes[ThreadLocalRandom.current().nextInt(nopes.length)]);
+                    embed.setDescription("It looks like you're trying to randomly ping a staff");
+                    embed.setFooter(donts[ThreadLocalRandom.current().nextInt(donts.length)]);
+                    message.getChannel().sendMessage(message.getUserAuthor().get().getMentionTag(),embed).thenAcceptAsync(msg -> api.getThreadPool().getScheduler().schedule((Callable<CompletableFuture<Void>>) msg::delete, 30, TimeUnit.MINUTES));
+                    message.getUserAuthor().get().sendMessage(embed);
+                    break;
+                }
+            }
+        }
+    }
+
+    private boolean userIsActive(long userId) {
+        if (!active.keySet().contains(userId)) {
+            return false;
+        }
+        return (Duration.between(active.get(userId), Instant.now()).toMinutes() <= 15);
     }
 
     public void logCensorMessage(Optional<User> user, String pattern, String chanId) {
