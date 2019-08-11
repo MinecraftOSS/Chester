4d0d826c8bc4f7025c3d9e4cbb83e6f08bc53a65
U-NIFLHEIM\Lax <LaxWasHere@PoweredByHate.net>
2019-08-11 03:47:37 -0700

    updatable cmd/perm db, cmd search now shows multiple results

--- src/main/java/org/moss/discord/commands/EssentialsXCommand.java
+++ src/main/java/org/moss/discord/commands/EssentialsXCommand.java
@@ -22,15 +22,43 @@ import java.util.Objects;
 
 public class EssentialsXCommand implements CommandExecutor {
 
-    private static JsonNode essxCommands;
-    private static JsonNode essxPermissions;
-    private static Map<String, List<String>> itemDb = new HashMap<>();
+
+    private static String itemdbURL = "https://raw.githubusercontent.com/EssentialsX/Essentials/2.x/Essentials/src/items.json";
+    private static String commanddbURL = "https://essinfo.xeya.me/index.php?page=commands&raw-data=true";
+    private static String permissionsdbURL = "https://essinfo.xeya.me/index.php?page=permissions&raw-data=true";
+
+    private JsonNode essxCommands;
+    private JsonNode essxPermissions;
+    private Map<String, List<String>> itemDb = new HashMap<>();
+
     private ObjectMapper mapper = new ObjectMapper();
+    private static final OkHttpClient client = new OkHttpClient.Builder().build();
 
     public EssentialsXCommand() {
         try {
-            essxCommands = mapper.readTree(new File("./essx_commands.json")).get(0).get("data");
+            essxCommands = mapper.readTree(new File("./essx_commands.json"));
+            JsonNode cmdb2 =  mapper.readTree(Objects.requireNonNull(client.newCall(new Request.Builder().url(commanddbURL).build()).execute().body()).string());
+            if (cmdb2.size() > essxCommands.size()) {
+                System.out.println("Updating Essentials CommandsDB.");
+                updateCommandsDB();
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println("Acquiring Essentials CommandsDB");
+            updateCommandsDB();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
             essxPermissions = mapper.readTree(new File("./essx_perms.json"));
+            JsonNode permdb2 =  mapper.readTree(Objects.requireNonNull(client.newCall(new Request.Builder().url(permissionsdbURL).build()).execute().body()).string());
+            if (permdb2.size() > essxPermissions.size()) {
+                System.out.println("Updating Essentials PermsDB.");
+                updatePermissionsDB();
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println("Acquiring Essentials PermissionsDB");
+            updatePermissionsDB();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -38,8 +66,8 @@ public class EssentialsXCommand implements CommandExecutor {
         try {
             JsonNode essxItemDB = mapper.readTree(new File("./essx_items.json"));
 
-            Request request = new Request.Builder().url("https://raw.githubusercontent.com/EssentialsX/Essentials/2.x/Essentials/src/items.json").build();
-            JsonNode essxItemDB2 =  mapper.readTree(Objects.requireNonNull(new OkHttpClient.Builder().build().newCall(request).execute().body()).string().replace("#version: ${full.version}", ""));
+            Request request = new Request.Builder().url(itemdbURL).build();
+            JsonNode essxItemDB2 =  mapper.readTree(Objects.requireNonNull(client.newCall(request).execute().body()).string().replace("#version: ${full.version}", ""));
 
             if (essxItemDB2.size() > essxItemDB.size()) {
                 System.out.println("Updating Essentials ItemDB.");
@@ -58,19 +86,17 @@ public class EssentialsXCommand implements CommandExecutor {
     @Command(aliases = {"!essc", ".essc"}, usage = "!essc <command>", description = "Show info about a EssentialsX command")
     public void onCommand(String[] args, User user, TextChannel textChannel) {
         if (args.length >= 1) {
-            JsonNode command = searchCommands(args[0]);
-            if (command != null) {
+            List<JsonNode> commands = searchCommands(args[0]);
+            if (commands.size() >= 1) {
                 EmbedBuilder embed = new EmbedBuilder();
                 embed.setColor(new Color(11825408));
-
                 embed.setTitle("Command Information").setUrl("https://essinfo.xeya.me/index.php?page=commands");
-                embed.addInlineField("Command", command.get("Command").asText());
-                embed.addInlineField("Usage", command.get("Syntax").asText());
-                embed.addInlineField("Module", command.get("Module").asText());
-                embed.addInlineField("Description", command.get("Description").asText());
-                embed.addField("Aliases", String.format("```%s```", command.get("Aliases").asText()));
-                embed.setFooter("Requested By | " + user.getName());
 
+                for (JsonNode command : commands) {
+                    embed.addInlineField(command.get("Command").asText(), String.format("```Usage: %s \n\nDescripton: %s \n\nAliases: %s```", command.get("Syntax").asText(), command.get("Description").asText(), command.get("Aliases").asText()));
+                }
+
+                embed.setFooter("Requested By | " + user.getName());
                 textChannel.sendMessage(user.getMentionTag(), embed);
             } else {
                 textChannel.sendMessage(new EmbedBuilder().setTitle("Command not found").setColor(Color.RED));
@@ -128,24 +154,19 @@ public class EssentialsXCommand implements CommandExecutor {
         }
     }
 
-    private JsonNode searchCommands(String param) {
-        for (int i = 0; i <= essxCommands.size() -1; i++) {
-            JsonNode command = essxCommands.get(i);
-            if (param.equalsIgnoreCase(command.get("Command").asText())) {
-                return command;
+    private List<JsonNode> searchCommands(String param) {
+        List<JsonNode> nodes = new ArrayList<>();
+        for (Iterator<String> it = essxCommands.fieldNames(); it.hasNext();) {
+            JsonNode command = essxCommands.get(it.next());
+            if (param.equalsIgnoreCase(command.get("Command").asText()) || command.get("Aliases").asText().contains(param)) {
+                nodes.add(command);
             }
         }
-        for (int i = 0; i < essxCommands.size(); i++) {
-            JsonNode command = essxCommands.get(i);
-            if (command.get("Aliases").asText().contains(param)) {
-                return command;
-            }
-        }
-        return null;
+        return nodes;
     }
 
     private List<JsonNode> searchPermissions(String param) {
-        java.util.List<JsonNode> nodes = new ArrayList<>();
+        List<JsonNode> nodes = new ArrayList<>();
         for (Iterator<String> it = essxPermissions.fieldNames(); it.hasNext();) {
             JsonNode permNode = essxPermissions.get(it.next());
             if (permNode.get("Command").asText().contains(param) || permNode.get("Permission").asText().contains(param)) {
@@ -163,6 +184,24 @@ public class EssentialsXCommand implements CommandExecutor {
             }
         }
         return result;
+    }
+
+    private void updateCommandsDB() {
+        try {
+            essxCommands = mapper.readTree(cleanUp(Objects.requireNonNull(client.newCall(new Request.Builder().url(commanddbURL).build()).execute().body()).string()));
+            mapper.writerWithDefaultPrettyPrinter().writeValue(new File("./essx_commands.json"), essxCommands);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void updatePermissionsDB() {
+        try {
+            essxPermissions = mapper.readTree(cleanUp(Objects.requireNonNull(client.newCall(new Request.Builder().url(permissionsdbURL).build()).execute().body()).string()));
+            mapper.writerWithDefaultPrettyPrinter().writeValue(new File("./essx_perms.json"), essxPermissions);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private void updateItemDB(JsonNode database) {
@@ -188,5 +227,9 @@ public class EssentialsXCommand implements CommandExecutor {
                 itemDb.get(essxItemDB.get(s).asText()).add(s);
             }
         }
+    }
+
+    private String cleanUp(String xeyamesMess) { //https://i.imgur.com/InxkMUu.png
+        return xeyamesMess.replace("&lt;", "<").replace("&gt;", ">");
     }
 }
