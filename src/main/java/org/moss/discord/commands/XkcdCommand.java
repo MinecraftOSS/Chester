173f84117b2b816b2ae346b75afe02c5e9196d05
U-NIFLHEIM\Lax <LaxWasHere@PoweredByHate.net>
2019-08-16 21:27:42 -0700

    add links

--- src/main/java/org/moss/discord/commands/XkcdCommand.java
+++ src/main/java/org/moss/discord/commands/XkcdCommand.java
@@ -28,8 +28,9 @@ public class XkcdCommand implements CommandExecutor {
             if (id != null) {
                 JsonNode node = xkcd_info(id);
                 EmbedBuilder embed = new EmbedBuilder().setColor(Color.YELLOW);
+                String link = node.get("link").asText();
                 embed.setTitle("xkcd: " + node.get("safe_title").asText());
-                embed.setUrl(String.format("https://xkcd.com/%s/", id));
+                embed.setUrl(link.equalsIgnoreCase("") ? String.format("https://xkcd.com/%s/", id) : link);
                 embed.setImage(node.get("img").asText());
                 embed.setFooter(node.get("alt").asText());
                 channel.sendMessage(user.getMentionTag(), embed);
