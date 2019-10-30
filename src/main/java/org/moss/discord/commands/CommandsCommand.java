7124fa52adbfac281114873b9dec9b85e9e87201
U-NIFLHEIM\Lax <LaxWasHere@PoweredByHate.net>
2019-10-30 01:37:06 -0700

    paginatin

--- src/main/java/org/moss/discord/commands/CommandsCommand.java
+++ src/main/java/org/moss/discord/commands/CommandsCommand.java
@@ -5,6 +5,10 @@ import de.btobastian.sdcf4j.CommandExecutor;
 import de.btobastian.sdcf4j.CommandHandler;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.entity.channel.TextChannel;
+import org.javacord.api.entity.message.embed.EmbedBuilder;
+import org.moss.discord.util.PagedEmbed;
+
+import java.awt.*;
 
 public class CommandsCommand implements CommandExecutor {
 
@@ -16,12 +20,11 @@ public class CommandsCommand implements CommandExecutor {
 
     @Command(aliases = {"!commands", ".commands"}, usage = "!commands", description = "shows all commands")
     public void onCommand(DiscordApi api, TextChannel channel, String[] args) {
-        String message = "```";
+        EmbedBuilder embed = new EmbedBuilder().setTitle("M.O.S.S Commands").setColor(Color.ORANGE);
+        PagedEmbed pagedEmbed = new PagedEmbed(channel, embed);
         for (CommandHandler.SimpleCommand command : handler.getCommands()) {
-            String cmd = String.format("%-35s %s", command.getCommandAnnotation().usage(), command.getCommandAnnotation().description());
-            message += cmd+"\n";
+            pagedEmbed.addField(command.getCommandAnnotation().usage(), "```"+command.getCommandAnnotation().description()+"```");
         }
-        message += "```";
-        channel.sendMessage(message);
+        pagedEmbed.build().join();
     }
 }
